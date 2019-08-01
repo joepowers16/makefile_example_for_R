89afd73cd69849a0253af47fb10567f021db9f8d
Joe Powers <joepowers16@gmail.com>
2019-07-31 22:07:26 -0700

    adds file path

--- file_paths.R
+++ file_paths.R
@@ -9,3 +9,4 @@ file_ds_mtcars <- fs::path(dir_data, "ds_mtcars.rds")
 file_ds_mt_agg <- fs::path(dir_data, "ds_mt_agg.rds")
 file_ds_long_name_to_demo_line_breaks <- 
 	fs::path(dir_data, "file_ds_long_name_to_demo_line_breaks.rds")
+file_ds_mt_temp <- fs::path(dir_data, "ds_mt_temp.rds")
