baf7bcf08ced30780c96872d87e9f3881ff92e6d
Joe Powers <joepowers16@gmail.com>
2019-08-31 14:31:43 -0700

    places ds_mt_agg.rds in regular data/ folder

--- file_paths.R
+++ file_paths.R
@@ -6,7 +6,7 @@ dir_reports <- fs::path(dir_cloud, "reports")
 
 file_ds_mt_raw <- fs::path(dir_raw, "ds_mt_raw.csv")
 file_ds_mtcars <- fs::path(dir_data, "ds_mtcars.rds")
-file_ds_mt_agg <- here::here("ds_mt_agg.rds")
+file_ds_mt_agg <- fs::path(dir_data, "ds_mt_agg.rds")
 file_ds_long_name_to_demo_line_breaks <- 
 	fs::path(dir_data, "ds_long_name_to_demo_line_breaks.rds")
 file_ds_mt_temp <- fs::path(dir_data, "ds_mt_temp.rds")
