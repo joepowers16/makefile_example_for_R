9ab505dff0b95ae14af27d851d95a0c7cb9b2d31
Joe Powers <joepowers16@gmail.com>
2019-10-12 14:12:54 -0700

    updates file paths

--- file_paths.R
+++ file_paths.R
@@ -3,7 +3,7 @@ dir_data <- here::here("data")
 dir_raw <- fs::path(dir_data, "raw")
 dir_reports <- here::here("reports")
 
-file_ds_mt_raw <- fs::path(dir_raw, "ds_mt_raw.csv")
+file_ds_mt_raw <- fs::path(dir_raw, "mtcars_data", "ds_mt_raw.csv")
 file_ds_mtcars <- fs::path(dir_data, "ds_mtcars.rds")
 file_ds_mt_agg <- fs::path(dir_data, "ds_mt_agg.rds")
 file_ds_long_name_to_demo_line_breaks <- 
